d9fd64aea46104870a6bd5350d248a76a8c378fc
irah rosete <irah.rosete@outlook.com>
2022-04-19 19:01:22 +1000

    add styles

--- pages/index.js
+++ pages/index.js
@@ -22,11 +22,16 @@ export default function Recipes({ recipes }) {
   console.log(recipes)
   return (
     <div className="recipe-list">
-      Recipe List
       {recipes.map((recipe) => 
       <RecipeCard key={recipe.sys.id} recipe={recipe}/>
-      
       )}
+      <style jsx>{`
+        .recipe-list {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap: 20px 60px;
+        }
+      `}</style>
     </div>
   )
 }
