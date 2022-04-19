aa7a09a99a0ad4bada40da628a5f1dc029827d60
irah rosete <irah.rosete@outlook.com>
2022-04-19 18:46:16 +1000

    add image and whitelist url

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,11 +1,15 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function RecipeCard({recipe}) {
     const { title, slug, cookingTime, thumbnail } = recipe.fields
   return (
       <div className="card">
           <div className="featured">
-              {/* image thumbnail*/}
+              <Image 
+                src={'https:' + thumbnail.fields.file.url}
+                width={thumbnail.fields.file.details.image.width}
+                height={thumbnail.fields.file.details.image.height}/>
           </div>
           <div className="content">
               <div className="info">
