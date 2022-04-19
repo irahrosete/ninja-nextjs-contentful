54d247f807bfd98e8246c3b6e4c51c762e8974ef
irah rosete <irah.rosete@outlook.com>
2022-04-19 18:27:32 +1000

    update recipe card component

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,5 +1,23 @@
+import Link from 'next/link'
+
 export default function RecipeCard({recipe}) {
+    const { title, slug, cookingTime, thumbnail } = recipe.fields
   return (
-    <div>{recipe.fields.title}</div>
+      <div className="card">
+          <div className="featured">
+              {/* image thumbnail*/}
+          </div>
+          <div className="content">
+              <div className="info">
+                  <h4>{title}</h4>
+                  <p>Takes approx {cookingTime} mins to make</p>
+              </div>
+              <div className="actions">
+                  <Link href={'/recipes/' + slug}>
+                      <a>Cook this</a>
+                  </Link>
+              </div>
+          </div>
+      </div>
   )
 }
