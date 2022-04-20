fd8abb506bb153e84de01b768c8d9f63b9faba53
irah rosete <irah.rosete@outlook.com>
2022-04-20 22:25:54 +1000

    apply redirect to 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,15 @@
 import Link from "next/link"
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 export default function NotFound() {
+  const router = useRouter()
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/')
+    }, 4000)
+  }, [])
   return (
     <div className="not-found">
         <h1>404</h1>
