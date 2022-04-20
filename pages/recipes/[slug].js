f45c54e219e777880f4e9918ec8cdf4a137331e6
irah rosete <irah.rosete@outlook.com>
2022-04-20 17:36:35 +1000

    add fallback and create skeleton component

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from 'contentful'
 import Image from 'next/image'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import Skeleton from '../../components/Skeleton'
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -20,7 +21,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths, // this is the same as paths: paths
-    fallback: false
+    fallback: true
   }
 }
 
@@ -38,6 +39,10 @@ export async function getStaticProps({ params }) {
 }
 
 export default function RecipeDetails({ recipe }) {
+  if (!recipe) {
+    return <Skeleton />
+  }
+
   const {featuredImage, title, cookingTime, ingredients, method} = recipe.fields
   return (
     <div>
